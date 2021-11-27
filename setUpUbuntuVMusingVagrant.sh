70a879c1ed85afef3fc46ed734aaf1063c6de81d
Nitin Mishra <nitin.mishra@knoldus.com>
2021-11-27 14:12:03 +0530

    Update repo

--- setUpUbuntuVMusingVagrant.sh
+++ setUpUbuntuVMusingVagrant.sh
@@ -2,8 +2,16 @@
 
 # Create a workspace, and inside that create two sub directories(it could be n as well)
 for i in centOS ubuntu; do mkdir -p workspace/$i; done
-cd ubuntu
+echo "============= workspace created =============="
+cd workspace/ubuntu
 #initialize your VM by mentioning box name
+echo "============= Initializing vm using ubuntu box ==============="
 vagrant init ubuntu/trusty64 
 #run the initialized box.
+echo "============= start and provision the vagrant environment =================="
 vagrant up
+echo "=========== Current Vagrant version =============="
+vagrant status --version
+# ssh to your vm
+echo "============= connect to your VM =================="
+vagrant ssh
